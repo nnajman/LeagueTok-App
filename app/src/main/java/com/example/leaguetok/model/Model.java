39dd22d1e472e106d018bf776fc50d29400296e8
Sarah-Lee <sarahlee.elhadad@gmail.com>
2021-03-24 19:30:16 +0200

    prevent app from crushing when moving between tabs

--- app/src/main/java/com/example/leaguetok/model/Model.java
+++ app/src/main/java/com/example/leaguetok/model/Model.java
@@ -35,7 +35,7 @@ public class Model {
             refreshAllOrigVideos(listener);
         }
         else {
-            listener.onComplete(null);
+            if(listener != null) listener.onComplete(null);
         }
 
         return origVideosList;
