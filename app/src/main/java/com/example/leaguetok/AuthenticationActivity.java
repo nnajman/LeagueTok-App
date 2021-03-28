60fdb93f846b2d3f51dacbf3fb462a2a685c8cec
tomer <tomer@LAPTOP-9RP2E9JV>
2021-03-28 17:55:57 +0300

    added authentication activity added sign in fragment xml

---
+++ app/src/main/java/com/example/leaguetok/AuthenticationActivity.java
@@ -0,0 +1,23 @@
+package com.example.leaguetok;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.view.View;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
