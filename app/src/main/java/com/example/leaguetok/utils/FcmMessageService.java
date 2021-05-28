c672c73b6cff4b0e9ca5a0903a923dd60361d5d4
Sarah-Lee <sarahlee.elhadad@gmail.com>
2021-05-28 19:04:07 +0300

    create custom notification layout

--- app/src/main/java/com/example/leaguetok/utils/FcmMessageService.java
+++ app/src/main/java/com/example/leaguetok/utils/FcmMessageService.java
@@ -1,6 +1,19 @@
 package com.example.leaguetok.utils;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.widget.RemoteViews;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.example.leaguetok.MainActivity;
+import com.example.leaguetok.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -12,8 +25,32 @@ public class FcmMessageService extends FirebaseMessagingService {
         //TODO: send new token to server
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        if (remoteMessage.getNotification() != null) {
+            // Set custom notification layout
+            RemoteViews remoteViews = new RemoteViews(getApplicationContext().getPackageName(), R.layout.notification);
+            remoteViews.setTextViewText(R.id.notification_title, remoteMessage.getNotification().getTitle());
+            remoteViews.setTextViewText(R.id.notification_text, remoteMessage.getNotification().getBody());
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+            String channel_id = "notification_channel";
+            NotificationCompat.Builder builder = new NotificationCompat
+                    .Builder(getApplicationContext(), channel_id)
+                    .setAutoCancel(true)
+                    .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                    .setOnlyAlertOnce(true)
+                    .setContentIntent(pendingIntent)
+                    .setContent(remoteViews);
+
+            ((NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE)).notify(0, builder.build());
+
+        }
     }
 }
